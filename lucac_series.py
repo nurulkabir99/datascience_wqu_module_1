00dcd1e82c11772924be59aaeee2721479f7efd6
Md Nurul Kabir <40609617+nurulkabir99@users.noreply.github.com>
2021-01-20 19:27:06 +0600

    Exercise No 2
    Find Lucas Lehmer Series

---
+++ lucac_series.py
@@ -0,0 +1,9 @@
+def lucas_lehmer(p):
+    ll_seq = [4]
+    if p>2:
+        for i in range(1, (p-2)+1):
+            n_i = ((ll_seq[i-1]) ** 2 - 2) % ((2 ** p) - 1)
+            ll_seq.append(n_i)
+    return ll_seq
+
+print(lucas_lehmer(61))
